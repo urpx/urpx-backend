99f0983c7f527f4f9bf6a5d31bf4c0cef6239b59
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2019-06-09 05:24:10 +0000

    Add process that register common blueprint

--- urpx/app.py
+++ urpx/app.py
@@ -1,6 +1,13 @@
 from flask import Flask
+from .common.views import blueprint as common_blueprint
 
 def create_app():
     app = Flask(__name__)
+    app.url_map.strict_slashes = False
+
+    register_blueprints(app)
 
     return app
+
+def register_blueprints(app):
+    app.register_blueprint(common_blueprint)
